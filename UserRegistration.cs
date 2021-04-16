025925d487374a4d456d4702d0a88e1eadd72d46
ok97 <omkhawshi0@gmail.com>
2021-04-17 02:28:29 +0530

    Added: Last Name Valid

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -2,7 +2,7 @@
 using System.Text.RegularExpressions;
 
 namespace User_Registration_Problem
-{  /*  UC1:- As a User need to enter a valid First Name - First name starts with Cap and has minimum 3 characters
+{  /*  UC2:- As a User need to enter a valid Last Name - Last name starts with Cap and has minimum 3 characters
    */
     class UserRegistration
     {
@@ -10,24 +10,34 @@ namespace User_Registration_Problem
         public static bool NameValidation(String firstName)
         {
             String pattern = "^[A-Z]{1}[a-z]{2,}$";  //Define Pattern
-
             Regex regex = new Regex(pattern);      //create object of the Regex class (its Regesx predefine class)
-            return regex.IsMatch(firstName);     //match in a specified input string 
+            return regex.IsMatch(firstName);         //match in a specified input string
         }
-        static void Main(string[] args)  //Main Method
+        static void Main(string[] args)    //Main Method
         {
-            Console.WriteLine("Welcome to User Registration");   //Print message       
+            Console.WriteLine("Welcome to User Registration!");   //Print message             
 
-            Console.WriteLine("Enter the First Name");  //Take user input
+            Console.WriteLine("Enter the First Name"); //Take user input
             String firstName = Console.ReadLine();     //Store input string veriable
-
-            if (NameValidation(firstName))  //Check firstname NameValidation method
+            if (NameValidation(firstName))           //Check firstname NameValidation method
             {
                 Console.WriteLine("Valid First Name");
             }
             else
             {
                 Console.WriteLine("Invalid First Name");
+            }
+
+            Console.WriteLine("\nEnter the Last Name"); // Take user input
+            String lastName = Console.ReadLine();             //Store input string veriable
+
+            if (NameValidation(lastName))             //Check lastName NameValidation method
+            {
+                Console.WriteLine("Valid Last Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Last Name");
             }
 
         }
