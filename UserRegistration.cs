0a76a717d501ebb240285ab9886ca27b18b234c7
ok97 <omkhawshi0@gmail.com>
2021-04-17 10:05:33 +0530

    Added: Mobile Number Validation

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -2,22 +2,28 @@
 using System.Text.RegularExpressions;
 
 namespace User_Registration_Problem
-{  /*  UC3:- As a User need to enter a valid email - E.g. abc.xyz@bl.co.in - Email has 3 mandatory parts 
-       (abc, bl & co) and 2 optional (xyz & in) with precise @ and . positions
+{  /*  UC4:- As a User need to follow pre -defined Mobile Format 
+       - E.g. 91 9919819801 - Country code follow by space and 10 digit number
    */
     class UserRegistration
     {
+        public static bool PhoneNumberValidation(String Pno)
+        {
+            String PPattern = @"^\+?\d{0,2}\-?\d{4,5}\-?\d{5,6}"; //Define Phone Number Pattern
+            Regex Pregex = new Regex(PPattern); //create object of the Regex class (its Regesx predefine class)
+            return Pregex.IsMatch(Pno);
+        }
         public static bool EmailValidation(String email)
         {
             String Epattern = @"^[a-z]+([-+*.]?[0-9a-z])*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$"; //Define Email Pattern
             Regex eregex = new Regex(Epattern); //create object of the Regex class (its Regesx predefine class)
-            return eregex.IsMatch(Epattern);
+            return eregex.IsMatch(email);
 
         }
         public static bool NameValidation(String firstName)
         {
             String pattern = "^[A-Z]{1}[a-z]{2,}$";  //Define Pattern
-            
+
 
             Regex regex = new Regex(pattern);      //create object of the Regex class (its Regesx predefine class)
             return regex.IsMatch(firstName);         //match in a specified input string and  return value
@@ -48,11 +54,11 @@ namespace User_Registration_Problem
             {
                 Console.WriteLine("Invalid Last Name");  //print Lastname Invalid
             }
-            
-            Console.WriteLine("Enter Email:- ");  // Take user input
-            string email = Console.ReadLine();      //Store input string veriable
 
-            if (EmailValidation(email))    //Check email NameValidation method
+            Console.WriteLine("\nEnter Email:- ");  // Take user input
+            string Epattern = Console.ReadLine();      //Store input string veriable
+
+            if (EmailValidation(Epattern))    //Check email NameValidation method
             {
                 Console.WriteLine("Valid Email");  //Print message input valid
             }
@@ -60,13 +66,17 @@ namespace User_Registration_Problem
             {
                 Console.WriteLine("Invalid Email");  //print Email Invalid
             }
-                
-            
 
-
-
+            Console.WriteLine("\nEnter Phone Number :- ");  // Take user input
+            string Pno = Console.ReadLine();      //Store input string veriable
+            if (PhoneNumberValidation(Pno))
+            {
+                Console.WriteLine("Valid Phone Number");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Phone Number");
+            }
         }
-        
     }
-
 }
