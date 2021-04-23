90dc93909a87aaacc52c8e999f73ced536818d20
ok97 <omkhawshi0@gmail.com>
2021-04-24 01:36:40 +0530

    Added: Test case Email Valid or Invalid

--- UnitTestUserRegistrationProblem/UnitTest1.cs
+++ UnitTestUserRegistrationProblem/UnitTest1.cs
@@ -21,7 +21,7 @@ namespace UnitTestUserRegistrationProblem
 
         //Invalid Names
         [TestMethod]
-        [DataRow("omp", "Invalid")]
+        [DataRow("omp", "Invalid")]  //add parameters using the [DataRow] attribute values from the [DataRow] attribute will be passed to the test method
         [DataRow("ok", "Invalid")]
         [DataRow(".dfgjs", "Invalid")]
         public void UnitTestPatternValidation_InvalidName(string FirstName, string expected)
@@ -40,8 +40,39 @@ namespace UnitTestUserRegistrationProblem
         }
 
 
+        // Valid Email 
+        [TestMethod]
+        [DataRow("omkhawshi@gmail.com", true)]   //add parameters using the [DataRow] attribute values from the [DataRow] attribute will be passed to the test method
+        [DataRow("abc-100@yahoo.com", true)]
+        //[DataRow("abc.@gmail.com", true)]
+        public void UnitTestPatternValidation_ValidEmail(string Email, bool expected)
+        {
+            string Epattern = @"^[a-z]+([-+*.]?[0-9a-z])*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$"; //Define Pattern
+            bool actual = Program.PatternValidation(Email, Epattern); //passing parameter
+
+            Assert.AreEqual(expected, actual);  // section verifies that the action of the method under test behaves as expected.
+        }
 
 
+        //Invalid Email
+        [TestMethod]
+        [DataRow(".@gmail.com", "Invalid")]  //add parameters using the [DataRow] attribute values from the [DataRow] attribute will be passed to the test method
+        [DataRow("abc123@.com.com", "Invalid")]        
+        [DataRow("abc.@gmail.com", "Invalid")]        
+        public void UnitTestPatternValidation_InvalidEmail(string Email, string expected)
+        {
+            try
+            {
+                string Epattern = @"^[a-z]+([-+*.]?[0-9a-z])*@[a-z0-9]+\.(\.?[a-z]{2,}){1,2}$"; //Define Pattern
+                bool actual = Program.PatternValidation(Email, Epattern); //passing parameter
+            }
+            catch (InvalidDataException ex)
+            {
+
+                Assert.AreEqual(expected, ex.Message); // section verifies that the action of the method under test behaves as expected.
+            }
+
+        }
 
 
     }
